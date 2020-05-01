8c848fcf6fc4db2cb3cd6b897988c6daf63a5eb1
ketan7705 <hi@example.com>
2020-05-01 22:49:21 +0530

    The commit for trafficMap app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import { fetchFromAPI } from './store/actions/api_actions';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStateToProps = state => {
+  return {
+    API: state.APIReducer
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    startFetch: () => dispatch(fetchFromAPI())
+  }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
+
+
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const sagaMiddleware = createSagaMiddleware();
+const logger = createLogger();
+
+export default function configureStore(initialState, history) {
+    let store = {};
+
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+    let createStoreWithMiddleware = composeEnhancers(
+        applyMiddleware(sagaMiddleware, logger)
+    )(createStore);
+
+    store = createStoreWithMiddleware(rootReducer, initialState);
+    return store;
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
